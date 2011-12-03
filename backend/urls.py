8eabd352df192852d07f615ed84bca215f9e3806
Matt Westcott <matt@west.co.tt>
2011-12-03 13:48:50 +0000

    add initial django app for backend

---
+++ backend/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'core.views.home', name='home'),
+    # url(r'^foo/', include('foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
+urlpatterns += staticfiles_urlpatterns()
