46ce047051be9fb4d00af9edcda9185dc72542e4
Matt Westcott <matt@west.co.tt>
2011-12-03 17:59:38 +0000

    return list of locations in product api and vice versa; also do some cosmetic fixing in admin

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,12 +2,9 @@ from django.conf.urls.defaults import *
 from piston.resource import Resource
 from api.handlers import *
 
-location_handler = Resource(LocationHandler)
-product_handler = Resource(ProductHandler)
-
 urlpatterns = patterns('',
-	url(r'^(\w+)/location/(\d+)/', location_handler),
-	url(r'^(\w+)/locations/', location_handler),
-	url(r'^(\w+)/product/(\d+)/', product_handler),
-	url(r'^(\w+)/products/', product_handler),
+	url(r'^(\w+)/location/(\d+)/', Resource(LocationHandler)),
+	url(r'^(\w+)/locations/', Resource(LocationListHandler)),
+	url(r'^(\w+)/product/(\d+)/', Resource(ProductHandler)),
+	url(r'^(\w+)/products/', Resource(ProductListHandler)),
 )
