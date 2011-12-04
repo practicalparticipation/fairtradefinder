73f0f446dd4427538c87b2102a430c7c8ea16f30
Tim Davies <tim@practicalparticipation.co.uk>
2011-12-04 11:48:45 +0000

    Creating a branch to explore wiring the design up

---
+++ frontend/templates/header-mobile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html> 
+	
+	<head> 
+		<title>Fairtrade Finder</title> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
+		<link rel="stylesheet" href="css/customer.css" />
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
+	</head> 
+	
+	<body> 
+
+		<div data-role="page">
+
+			<div data-role="header">
+				<h1>Fairtrade Finder</h1>
+			</div>
